a9e84d7b7c9b3fc1197859be45751bf0e769cea0
Automatron <82890199+Luis-Munu@users.noreply.github.com>
2024-08-15 10:24:35 +0200

    Updated subscribe button detection for new layout

--- subscription.py
+++ subscription.py
@@ -6,10 +6,13 @@ from contextlib import contextmanager
 
 #import chromedriver_autoinstaller
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Change the language here, Spanish (es) or English (en)
 language = "es"
@@ -24,10 +27,15 @@ def get_driver():
     options.add_experimental_option("useAutomationExtension", False)
     options.add_experimental_option("excludeSwitches",["enable-automation"])
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-web-security')
     options.add_argument(f'--lang={language}')
     
     #chromedriver_autoinstaller.install() 
-    driver = webdriver.Chrome(executable_path='config/chromedriver.exe', options=options)
+    #driver = webdriver.Chrome(executable_path='config/chromedriver.exe', options=options)
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
     try:
         yield driver
     finally:
@@ -68,7 +76,12 @@ def sign_in(driver, username, password):
 def channel_subscription(driver, url):
     driver.get(url)
     try:
-        subscribe_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "subscribe-button")))
+        #subscribe_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "subscribe-button")))
+        subscribe_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, ".yt-spec-button-shape-next.yt-spec-button-shape-next--filled.yt-spec-button-shape-next--mono")
+            )
+        )
         if subscribe_button.text == elements["labels"]["subscribe_button"][language]:
             subscribe_button.click()
             logging.info("Subscribed to channel: " + url)
